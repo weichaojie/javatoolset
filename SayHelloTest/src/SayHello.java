d2416f20c6a548f34a0f5bd4687092be531413f9
weichaojie <18971302695@189.cn>
2016-12-31 22:59:39 +0800

    second commit test
    Signed-off-by: weichaojie <18971302695@189.cn>

--- SayHelloTest/src/SayHello.java
+++ SayHelloTest/src/SayHello.java
@@ -5,6 +5,7 @@ public class SayHello {
 public static void main(String [] args){
     System.out.println("How do you do");
     System.out.println("Fine, Thank you, And you?");
+    System.out.println("Goodbye!");
 }
 }
 
