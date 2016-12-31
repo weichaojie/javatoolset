e314c74a50998beb9ee32e02155a85db4be4987c
weichaojie <18971302695@189.cn>
2016-12-31 22:49:29 +0800

    First commit Signed-off-by: weichaojie <18971302695@189.cn>

---
+++ SayHelloTest/src/SayHello.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2016/12/31.
+ */
+public class SayHello {
+public static void main(String [] args){
+    System.out.println("How do you do");
+    System.out.println("Fine, Thank you, And you?");
+}
+}
+
