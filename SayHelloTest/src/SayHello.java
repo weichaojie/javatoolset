82deac3ec514be1f0e2c1a05b461e459805d56bf
weichaojie <18971302695@189.cn>
2016-12-31 23:42:57 +0800

    add commit message

--- SayHelloTest/src/SayHello.java
+++ SayHelloTest/src/SayHello.java
@@ -1,11 +1,14 @@
 /**
  * Created by Administrator on 2016/12/31.
  */
+import java.lang.*;
+
 public class SayHello {
 public static void main(String [] args){
     System.out.println("How do you do");
     System.out.println("Fine, Thank you, And you?");
     System.out.println("Goodbye!");
+    System.out.println("endln");
 }
 }
 
