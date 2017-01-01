3c49cacd7e078dfc6d2d8ef0d1a3bac770c065b5
weichaojie <18971302695@189.cn>
2017-01-01 21:36:05 +0800

    update throw exception

--- SayHelloTest/src/SayHello.java
+++ SayHelloTest/src/SayHello.java
@@ -4,14 +4,13 @@
 import java.lang.*;
 
 public class SayHello {
-public static void main(String [] args) {
+public static void main(String [] args) throws Exception
+{
     System.out.println("How do you do");
     System.out.println("Fine, Thank you, And you?");
     System.out.println("Goodbye!");
     System.out.println("endln");
     System.out.println("add a code line to test version control system");
-
-    SayHelloTest test;
 }
 
 }
