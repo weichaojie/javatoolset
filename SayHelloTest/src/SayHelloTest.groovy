d795bcd48f74b74adf8d481e78dd68a405aa5432
weichaojie <18971302695@189.cn>
2017-01-01 20:55:39 +0800

    add groovy test case code

--- SayHelloTest/src/SayHelloTest.groovy
+++ SayHelloTest/src/SayHelloTest.groovy
@@ -1,5 +1,17 @@
 /**
  * Created by Administrator on 2017/1/1.
  */
+import groovy.util.GroovyTestCase
+import org.codehaus.groovy.ant.Groovy;
+
 class SayHelloTest extends groovy.util.GroovyTestCase {
+
+    static main(){
+        print("run main function ");
+        runTest();
+    }
+    protected void runTest() throws Throwable {
+        super.runTest()
+        print("run groovy test");
+    }
 }
