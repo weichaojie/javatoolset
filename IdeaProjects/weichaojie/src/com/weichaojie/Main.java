44b90620bcede918c42c3c427ddd506c20f6ef3a
weichaojie <weichaojie@huawei.com>
2017-11-18 15:50:20 +0800

    添加数据 Signed-off-by: weichaojie <weichaojie@huawei.com>

--- IdeaProjects/weichaojie/src/com/weichaojie/Main.java
+++ IdeaProjects/weichaojie/src/com/weichaojie/Main.java
@@ -1,23 +1,41 @@
 package com.weichaojie;
 
+import java.lang.Thread;
 
 import org.apache.log4j.*;
+import util.HttpClientUtil;
 
 public class Main {
 
     private static Logger logger = Logger.getLogger(Main.class);
+    private static String urlAddr = "http://www.baidu.com/";
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+//        TestMultiGetHttpData();
 
         TestImportLogforJava();
 
     }
 
+    private static void TestMultiGetHttpData() {
+        for (int i = 0; i < Long.MAX_VALUE; i++) {
+            try {
+                String result = HttpClientUtil.doGet(urlAddr);
+                logger.info(result);
+                Thread.sleep(2000);
+
+            } catch (Exception e) {
+                logger.warn(e.getLocalizedMessage());
+            }
+        }
+    }
+
     private static void TestImportLogforJava() {
         logger.debug("This is debug message.");
-        logger.info("This is info message.");
-        logger.error("This is error message.");
+        logger.warn("This is a warning message");
+        //        logger.info("This is info message.");
+//        logger.error("This is error message.");
 
     }
 
